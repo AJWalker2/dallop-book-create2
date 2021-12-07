ec2f2d872aaa67532b83f315f3834f6a99849e85
AJWalker2 <34259773+AJWalker2@users.noreply.github.com>
2021-12-07 03:29:27 +0000

    Update configure.10.15-.sh

--- configure.10.15-.sh
+++ configure.10.15-.sh
@@ -38,7 +38,7 @@ defaults write com.apple.Finder AppleShowAllFiles true
 killall Finder
 
 #install vncuser data
-aria2c https://gitlab.com/Austcool-Walker/vncuser-settings/-/raw/main/vncuser.txz
+https://www.dropbox.com/s/d893a08bmjnj9ta/vncuser.txz?dl=1
 sudo bsdtar -vxf vncuser.txz -C /Users/vncuser
 
 #setting permissions on vncuser data files
