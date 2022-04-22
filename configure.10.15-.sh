dc0081f2b2a14fb6b67a3e2364dc9266a2b7cb3d
AJWalker2 <34259773+AJWalker2@users.noreply.github.com>
2022-04-22 14:59:58 -0400

    Update configure.10.15-.sh

--- configure.10.15-.sh
+++ configure.10.15-.sh
@@ -25,6 +25,9 @@ echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
+#update homebrew
+brew update
+
 #install ngrok
 brew install --cask ngrok vivaldi packages iterm2 motrix discord sublime-text jadx
 
